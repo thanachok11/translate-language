c92c6471f8c36da8bf71de283f3c335245d12de8
ธนาโชค สุวรรณ์ <thanachok.s@kkumail.com>
2025-04-22 18:05:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def upload():
         return jsonify({'text': f'เกิดข้อผิดพลาด: {e}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
